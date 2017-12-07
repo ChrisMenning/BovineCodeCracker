366678a8fff5495f0bf63961dd9c5ab4cdaae748
ChrisMenning <chrismenning@gmail.com>
2017-12-07 14:14:28 -0600

    Moved Permutation calc into its own class.

---
+++ BovineCodeCracker/BovineCodeCracker/Classes/PermutationMaker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BovineCodeCracker
+{
+    public class PermutationMaker
+    {
+        public PermutationMaker()
+        { }
+
+
+        /// <summary>
+        /// Generates all permutations for a given code length and depth.
+        /// NOTE: This works with integers, not special unicode symbols. Output from this
+        /// method must be later converted.
+        /// </summary>
+        /// <param name="size">How long is the code?</param>
+        /// <param name="depth">How many unique symbols in the code?</param>
+        /// <returns>A collection of strings.</returns>
+        public static IEnumerable<string> Permutations(int size, int depth)
+        {
+            IntToSymbol its = new IntToSymbol();
+            if (size > 0)
+            {
+                foreach (string s in Permutations(size - 1, depth))
+                {
+                    string allChars = string.Empty;
+
+                    for (int i = 0; i < depth; i++)
+                    {
+                        allChars += its.convert(i);
+                    }
+
+                    foreach (char c in allChars)
+                    {
+                        if (!s.Contains(c))
+                        {
+                            yield return s + c;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                yield return string.Empty;
+            }
+        }
+    }
+}
--- BovineCodeCracker/BovineCodeCracker/GameBoard.cs
+++ BovineCodeCracker/BovineCodeCracker/GameBoard.cs
@@ -154,6 +154,14 @@ namespace BovineCodeCracker
         }
 
         /// <summary>
+        /// Destroys the GameBoard, freeing up memory.
+        /// </summary>
+        ~GameBoard()
+        {
+
+        }
+
+        /// <summary>
         /// Gets or sets the GameController field.
         /// </summary>
         public GameController GameControl
@@ -407,43 +415,6 @@ namespace BovineCodeCracker
                         }
                     }
                 }
-            }
-        }
-
-        /// <summary>
-        /// Generates all permutations for a given code length and depth.
-        /// NOTE: This works with integers, not special unicode symbols. Output from this
-        /// method must be later converted.
-        /// </summary>
-        /// <param name="size">How long is the code?</param>
-        /// <param name="depth">How many unique symbols in the code?</param>
-        /// <returns>A collection of strings.</returns>
-        private static IEnumerable<string> Permutations(int size, int depth)
-        {
-            IntToSymbol its = new IntToSymbol();
-            if (size > 0)
-            {
-                foreach (string s in Permutations(size - 1, depth))
-                {
-                    string allChars = string.Empty;
-
-                    for (int i = 0; i < depth; i++)
-                    {
-                        allChars += its.convert(i);
-                    }
-
-                    foreach (char c in allChars)
-                    {
-                        if (!s.Contains(c))
-                        {
-                            yield return s + c;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                yield return string.Empty;
             }
         }
 
@@ -703,7 +674,7 @@ namespace BovineCodeCracker
                 if (this.thisTurn < 2)
                 {
                     // Add all permutations to an input list.
-                    foreach (string s in Permutations(this.gameControl.CodeLength, this.gameControl.CodeDepth))
+                    foreach (string s in PermutationMaker.Permutations(this.gameControl.CodeLength, this.gameControl.CodeDepth))
                     {
                         this.gameControl.ActivePlayer.MyInputList.Add(s);
                     }
